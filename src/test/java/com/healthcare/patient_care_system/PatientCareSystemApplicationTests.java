271ddfd4cef44ead8138f34955c190f4fd7f74ee
vikalpsareen <sareen.vikalp@gmail.com>
2025-11-07 14:16:09 -0500

    project setup

---
+++ src/test/java/com/healthcare/patient_care_system/PatientCareSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthcare.patient_care_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientCareSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
